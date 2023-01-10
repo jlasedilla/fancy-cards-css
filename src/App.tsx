52671557118509206b5756aed1c4deac04742cd7
Jay <jlordasedilla@gmail.com>
2023-01-10 14:24:15 +0800

    add translation utility

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Card } from '~/components/card';
 import { CardGallery } from './components/card-gallery';
 import { CardStack } from './components/card-stack';
 
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
+
 function App() {
     return (
         <div className="App">
